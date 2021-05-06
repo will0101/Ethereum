498a692206017aa07101496e661660c7465f0bc1
William Bogans <williambogans98@yahoo.com>
2021-05-06 16:41:19 -0400

    Deployment woes

---
+++ inbox/deploy.js
@@ -0,0 +1,35 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+
+
+const provider = new HDWalletProvider(
+  "aware tumble syrup acid garden stem own expire furnace better solution tail", 
+"https://rinkeby.infura.io/v3/3f612d9efc874cf180d3c90defd4688a"
+);
+
+const web3 = new Web3(provider)
+
+
+const deploy = async () => {
+  try {
+
+    const accounts = await web3.eth.getAccounts()
+    
+    console.log(`Deploying from account, ${accounts[0]}`)
+    
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data:'0x' + bytecode, arguments: ["Howdy!"]})
+    .send({
+      gas: '1000000',
+      gasPrice: web3.utils.toWei('2','gwei'),
+      from:accounts[0]})
+      
+      console.log(`Contract deployed to: ${result.options.address}`)
+    } catch (error) {
+      console.log(error.message)
+    }
+}
+
+deploy()
